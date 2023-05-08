0c3e496467211c97b9ce7ad90b99b8ea58f1648e
AyanaZaire <ayanazaire@gmail.com>
2023-05-08 11:05:54 -0400

    alt text for images

--- index.js
+++ index.js
@@ -4,16 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     dragElement(document.getElementById("image-1"))
     dragElement(document.getElementById("image-2"))
     dragElement(document.getElementById("image-3"))
-    // getDraggableElement()
 })
-
-// function getDraggableElement () {
-//     document.addEventListener("click", (event) => {
-//         console.log(event.target);
-//         console.log(document.getElementById("quotes"));
-//         dragElement(event.target)
-//     })
-// }
 
 function newQuoteHandler() {
     let button = document.getElementById("quote-button")
@@ -37,13 +28,7 @@ function displayQuote(quote) {
 
 function dragElement(elmnt) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    if (document.getElementById(elmnt.id + "header")) {
-      // if present, the header is where you move the DIV from:
-      document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-    } else {
-      // otherwise, move the DIV from anywhere inside the DIV:
-      elmnt.onmousedown = dragMouseDown;
-    }
+    elmnt.onmousedown = dragMouseDown;
   
     function dragMouseDown(e) {
       e = e || window.event;
